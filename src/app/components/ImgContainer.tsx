e10c6b7b20c5509a0b95eba8cea1abd843947ded
djimenezchocron <djimenezweb@gmail.com>
2023-09-07 11:43:42 +0200

    lesson 9

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -1,22 +1,32 @@
 import type { Photo } from '@/models/Images';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
   photo: Photo;
 };
 
 export default function ImgContainer({ photo }: Props) {
+  const ratio = photo.height / photo.width;
+  const galHeight = Math.ceil(250 * ratio);
+  const span = Math.ceil(galHeight / 10) + 1;
+
   return (
-    <div className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group">
-      <Image
-        src={photo.src.large}
-        alt={photo.alt}
-        fill={true}
-        className="object-cover group-hover:opacity-70"
-        sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
-        placeholder="blur"
-        blurDataURL={photo.blurredDataUrl}
-      />
+    <div className="w-[250px] justify-self-center" style={{ gridRow: `span ${span}` }}>
+      <Link href={photo.url} target="_blank" className="grid-place-content-center">
+        <div className="rounded-xl overflow-hidden group">
+          <Image
+            src={photo.src.large}
+            alt={photo.alt}
+            width={250}
+            height={photo.height}
+            className="group-hover:opacity-70"
+            sizes="250px"
+            placeholder="blur"
+            blurDataURL={photo.blurredDataUrl}
+          />
+        </div>
+      </Link>
     </div>
   );
 }
